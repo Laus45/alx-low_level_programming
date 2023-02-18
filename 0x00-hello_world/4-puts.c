caca57ce66a9419654292c9652cbdef3f4c5cfc5
Laus <laus.melembe@gmail.com>
2023-02-18 10:32:43 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - Um programa que imprime uma linha usando a função puts
+ * Return: 0 (O programa deve retornar zero)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
