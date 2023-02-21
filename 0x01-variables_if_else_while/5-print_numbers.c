48623156f66482083ef951ab381e7f51335b1711
Laus <laus.melembe@gmail.com>
2023-02-21 14:02:58 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Base 10  numbers
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	printf("%d", n);
+	printf("\n");
+
+	return (0);
+}
