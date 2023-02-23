902b33b42535bdfa9cfa5089ddd42fb40b8ddce0
Laus <laus.melembe@gmail.com>
2023-02-23 13:38:06 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ * Return (0)
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
