9c8c24d997cc5707e011b16bb83ee1f49271db5a
Laus <laus.melembe@gmail.com>
2023-02-24 17:41:00 -0800

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints to 98
+ * @n: the starting number
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n != 98)
+			printf("%d, ", n);
+			else
+			printf("%d\n", n);
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n != 98)
+			printf("%d, ", n);
+			else
+			printf("%d\n", n);
+		}
+	}
+}
