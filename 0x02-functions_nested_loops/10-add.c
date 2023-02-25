b50f30d4238a1019ac4b9303eb9d8f6d68668661
Laus <laus.melembe@gmail.com>
2023-02-24 16:56:31 -0800

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: the sum result res
+ */
+
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
