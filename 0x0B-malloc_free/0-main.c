b42ac2003d76346735f9b82d1b9e482b6a37ad63
Laus <laus.melembe@gmail.com>
2023-03-25 09:45:20 -0700

    malloc, free

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -6,9 +6,9 @@
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- *
  * Return: Nothing.
  */
+
 void simple_print_buffer(char *buffer, unsigned int size)
 {
     unsigned int i;
@@ -32,9 +32,9 @@ void simple_print_buffer(char *buffer, unsigned int size)
 
 /**
  * main - check the code for ALX School students.
- *
  * Return: Always 0.
  */
+
 int main(void)
 {
     char *buffer;
